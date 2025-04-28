7ecde84f490409fcbdd223310f1aaf0a67b7a1be
Shiro Kuriwaki <shirokuriwaki@gmail.com>
2025-04-28 14:01:01 -0400

    Make version argument simpler

--- man-roxygen/version.R
+++ man-roxygen/version.R
@@ -1,11 +1,10 @@
 #' @param version A character specifying a version of the dataset.
 #'  This can be of the form `"1.1"` or `"1"` (where in `"x.y"`, x is a major
-#'  version and y is an optional minor version), or
-#'  `":latest"` (the default, the latest published version).
-#'  We recommend using the number format so that
-#'  the function stores a cache of the data (See \code{\link{cache_dataset}}).
-#'  If the user specifies a `key` or `DATAVERSE_KEY` argument, they can access the
-#'  draft version by `":draft"` (the current draft) or `":latest"` (which will
-#'  prioritize the draft over the latest published version.
+#'  version and y is an optional minor version). As of v0.3.14, setting a version
+#'  in this way will cache the dataset (See example in \code{\link{cache_dataset}})
+#'  so that it will not re-download the file the second time and read from the cache.
 #'  Finally, set `use_cache = "none"` to not read from the cache and re-download
 #'  afresh even when `version` is provided.
+#'  If the user specifies a `key` or `DATAVERSE_KEY` argument, they can access the
+#'  draft version by `":draft"` (the current draft) or `":latest"` (which will
+#'  prioritize the draft over the latest published version).
